566047a080a85eddfd863ee1d199c737226b894b
Amandeep Awasthi <aman01awasthi@gmail.com>
2026-04-28 02:09:34 +0530

    feat: persist expenses to localStorage with lazy initializer

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import './App.css'
-import ExpenseForm from './components/ExpenseForm'
-import ExpenseList from './components/ExpenseList';
-import ExpenseTotal from './components/ExpenseTotal';
-import CategoryFilter from './components/CategoryFilter';
+import { useEffect, useState } from "react";
+import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseTotal from "./components/ExpenseTotal";
+import CategoryFilter from "./components/CategoryFilter";
 
 function App() {
-  const [expenses, setExpenses] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState('');
+  const [expenses, setExpenses] = useState(() => {
+    const stored = localStorage.getItem("expenses");
+    return stored ? JSON.parse(stored) : [];
+  });
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   const handleAddExpense = (newExpense) => {
-    setExpenses(prev => [...prev, {...newExpense, amount: Number(newExpense.amount), id: Date.now()}]);
-  }
-  console.log(expenses);
+    setExpenses((prev) => [
+      ...prev,
+      { ...newExpense, amount: Number(newExpense.amount), id: Date.now() },
+    ]);
+  };
 
-  const handleDeleteExpense = (id)=>{
-    setExpenses(expenses.filter((expense)=> expense.id !== id))
-  }
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
 
-  const filteredCategory = selectedCategory ? expenses.filter((expense) => expense.category === selectedCategory) : expenses;
+  const handleDeleteExpense = (id) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
+
+  const filteredCategory = selectedCategory
+    ? expenses.filter((expense) => expense.category === selectedCategory)
+    : expenses;
 
   return (
     <>
-      <ExpenseForm onAddExpense={handleAddExpense}/>
-      <br/>
-      <CategoryFilter onCategoryChange={setSelectedCategory} selectedCategory={selectedCategory}/>
-      <ExpenseList data={filteredCategory} del={handleDeleteExpense}/>
-      <ExpenseTotal data={expenses}/>
+      <ExpenseForm onAddExpense={handleAddExpense} />
+      <br />
+      <CategoryFilter
+        onCategoryChange={setSelectedCategory}
+        selectedCategory={selectedCategory}
+      />
+      <ExpenseList data={filteredCategory} del={handleDeleteExpense} />
+      <ExpenseTotal data={expenses} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
