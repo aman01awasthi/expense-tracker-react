f517125ced740206e98b53a6bf47ce80bbb3e70a
Amandeep Awasthi <aman01awasthi@gmail.com>
2026-05-10 19:07:25 +0530

    feat: apply theme context across ExpenseForm and ExpenseItem

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,11 +1,13 @@
+import { useTheme } from "../context/ThemeContext";
 const ExpenseItem = ({data, del, onEditExpense}) => {
+    const {theme} = useTheme();
     return(
-        <div className="flex items-center justify-between bg-white rounded-xl shadow-sm p-4 mb-3">
+        <div className={`flex items-center justify-between rounded-xl shadow-sm p-4 mb-3 ${theme === 'light' ? 'bg-white' : 'bg-gray-700'}`}>
             <div className="flex-1">
-                <h3 className="font-semibold text-gray-800">{data.name}</h3>
-                <p className="text-sm text-gray-500">{data.category}</p>
+                <h3 className={`font-semibold ${theme === 'light' ? 'text-gray-800' : 'text-white'}`}>{data.name}</h3>
+                <p className={`text-sm text-gray-500 ${theme === 'light' ? 'text-gray-800' : 'text-white'}`}>{data.category}</p>
             </div>
-            <p className="font-bold text-gray-800 mr-6">₹{data.amount}</p>
+            <p className={`font-bold mr-6 ${theme === 'light' ? 'text-gray-800' : 'text-white'}`}>₹{data.amount}</p>
             <div className="flex gap-2">
                 <button type="button" onClick={()=> onEditExpense(data)} className="bg-yellow-400 text-white px-3 py-1 rounded-lg hover:bg-yellow-500 text-sm">Edit</button>
                 <button type="button" onClick={()=> del(data.id)} className="bg-red-500 text-white px-3 py-1 rounded-lg hover:bg-red-600 text-sm">Delete</button>
