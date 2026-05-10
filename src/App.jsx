504073757dc5ee3d4219f15e4705987962c8492a
Amandeep Awasthi <aman01awasthi@gmail.com>
2026-05-11 02:49:15 +0530

    fix: Fake User Login Access

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import ExpenseTotal from "./components/ExpenseTotal";
 import CategoryFilter from "./components/CategoryFilter";
 import Login from "./pages/Login";
 import { useTheme } from "./context/ThemeContext";
-import { Sun, Moon } from 'lucide-react';
-import {useLogin} from "./context/AuthContext";
+import { Sun, Moon } from "lucide-react";
+import { useLogin } from "./context/AuthContext";
 
 function App() {
-  const {user} = useLogin();
-  const {theme, setTheme} = useTheme();
+  const { user } = useLogin();
+  const { theme, setTheme } = useTheme();
   const [expenses, setExpenses] = useState(() => {
     const stored = localStorage.getItem("expenses");
     return stored ? JSON.parse(stored) : [];
@@ -56,32 +56,43 @@ function App() {
 
   return (
     <>
-    {!user ? <Login/> :
-      <div className={`min-h-screen p-6 ${theme === 'light' ? 'bg-gray-100' : 'bg-gray-900'}`}>
-        <button className="fixed bottom-6 right-6 bg-blue-500 text-white p-3 rounded-full shadow-lg hover:bg-blue-600 transition" onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-          {theme === 'light' ? <Moon size={20} /> : <Sun size={20} color="white"/>}
-        </button>
-        
-        <ExpenseForm
-          key={editExpense ? editExpense.id : "new"}
-          onAddExpense={handleAddExpense}
-          onEditExpense={handleUpdateExpense}
-          editExpense={editExpense}
-        />
-        <br />
-        <CategoryFilter
-          onCategoryChange={setSelectedCategory}
-          selectedCategory={selectedCategory}
-        />
-        <ExpenseList
-          data={filteredCategory}
-          del={handleDeleteExpense}
-          onEditExpense={handleEditExpense}
-          editExpense={editExpense}
-        />
-        <ExpenseTotal data={expenses} />
-      </div>
-}
+      {!user ? (
+        <Login />
+      ) : (
+        <div
+          className={`min-h-screen p-6 ${theme === "light" ? "bg-gray-100" : "bg-gray-900"}`}
+        >
+          <button
+            className="fixed bottom-6 right-6 bg-blue-500 text-white p-3 rounded-full shadow-lg hover:bg-blue-600 transition"
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+          >
+            {theme === "light" ? (
+              <Moon size={20} />
+            ) : (
+              <Sun size={20} color="white" />
+            )}
+          </button>
+
+          <ExpenseForm
+            key={editExpense ? editExpense.id : "new"}
+            onAddExpense={handleAddExpense}
+            onEditExpense={handleUpdateExpense}
+            editExpense={editExpense}
+          />
+          <br />
+          <CategoryFilter
+            onCategoryChange={setSelectedCategory}
+            selectedCategory={selectedCategory}
+          />
+          <ExpenseList
+            data={filteredCategory}
+            del={handleDeleteExpense}
+            onEditExpense={handleEditExpense}
+            editExpense={editExpense}
+          />
+          <ExpenseTotal data={expenses} />
+        </div>
+      )}
     </>
   );
 }
