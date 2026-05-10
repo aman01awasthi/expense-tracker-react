a8064c8fcd242a78380cb106c80ae41979bb0a3f
Amandeep Awasthi <aman01awasthi@gmail.com>
2026-05-10 19:23:51 +0530

    feat: theme context applied to CategoryFilter and ExpenseTotal

--- src/components/ExpenseTotal.jsx
+++ src/components/ExpenseTotal.jsx
@@ -1,6 +1,8 @@
+import { useTheme } from "../context/ThemeContext";
 const ExpenseTotal = ({data}) =>{
+    const {theme} = useTheme();
     return(
-        <div className="bg-blue-500 text-white rounded-xl p-4 mt-4 text-center w-max">
+        <div className={`rounded-xl p-4 mt-4 text-center w-max ${theme === 'light' ? 'bg-blue-500 text-white' : 'bg-white text-gray-800'}`}>
             <h2 className="text-xl font-bold">Total Expense: ₹{data.reduce((total, current)=> (total + current.amount),0)}</h2>
         </div>
     )
