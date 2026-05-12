eed0de3fa2db8767bfb2d683ba86ad3e0c87ec43
Amandeep Awasthi <aman01awasthi@gmail.com>
2026-05-13 02:57:47 +0530

    feat: add useLocalStorage custom hook

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+
+const useLocalStorage = (key, initialValue) => {
+
+    const [storedValue, setStoredValue] = useState(() => {
+        const stored = localStorage.getItem(key);
+        return stored ? JSON.parse(stored) : initialValue;
+    });
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(storedValue))
+    },[key, storedValue]);
+
+    return [storedValue, setStoredValue];
+}
+
+export default useLocalStorage;
