97ed1b2353cd92c714c27147f404c05542bbbb2e
Amandeep Awasthi <aman01awasthi@gmail.com>
2026-05-21 06:25:43 +0530

    refactor: removed unused and excess code from App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
